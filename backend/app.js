f08d9d50a6f6fb23de101df539f6d1758135860c
Ajay7616 <u.ajaykumar7616@gmail.com>
2026-08-03 01:39:14 +0530

    API Url dded

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "personal-book-manager-ajay.vercel.app",
         credentials: true,
     })
 );
